4028ed680dd79d8ad0d27d92fd4a861208aecc34
Zara2ctra <zara2ctra@gmail.com>
2023-08-22 22:52:11 +0400

    initial: initial react project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-React.render(
-    <App />
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
 );
